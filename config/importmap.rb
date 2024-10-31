3ed8b85fcd31ed0464959f8046ab4a097d84483e
Steve Taylor <sctaylor@stanford.edu>
2024-10-31 16:11:42 -0700

    Add importmap support

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+pin_all_from File.expand_path('../app/assets/javascripts/openseadragon', __dir__), under: 'openseadragon'
+pin_all_from File.expand_path('../vendor/assets/javascripts', __dir__)
