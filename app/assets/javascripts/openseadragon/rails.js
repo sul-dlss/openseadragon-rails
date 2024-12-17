4a1fb61274e191bdd84eb660612fec4043f93d1e
Justin Coyne <jcoyne@justincoyne.com>
2024-12-17 13:44:52 -0600

    Decouple from Sprockets
    Use dependencies in NPM

--- app/assets/javascripts/openseadragon/rails.js
+++ app/assets/javascripts/openseadragon/rails.js
@@ -1,32 +1,14 @@
-//= require openseadragon/jquery
+import 'openseadragon/dom'
 
-(function($) {
+(function() {
   function initOpenSeadragon() {
-    $('picture[data-openseadragon]:not(:has(.openseadragon-container))').openseadragon();
+    document.querySelectorAll('picture[data-openseadragon]:not(:has(.openseadragon-container))').openseadragon();
   }
-
-  const jquery3 = parseInt($.fn.jquery.split('.')[0]) >= 3;
-  let handler = 'ready';
 
   if (typeof Turbo !== 'undefined') {
-    handler = 'turbo:load turbo:frame-load';
-  } else if (typeof Turbolinks !== 'undefined' && Turbolinks.supported) {
-    // Turbolinks 5
-    if (Turbolinks.BrowserAdapter) {
-      handler = 'turbolinks:load';
-    } else {
-      // Turbolinks < 5
-      handler = 'page:load ready';
-    }
+    addEventListener("turbo:load", () => initOpenSeadragon())
+    addEventListener("turbo:frame-load", () => initOpenSeadragon())
+  } else {
+    addEventListener('load', () => initOpenSeadragon())
   }
-
-  // Support for $(document).on( "ready", handler ) was removed in jQuery 3
-  if (jquery3 && handler.includes('ready')) {
-    handler = handler.replace('ready', '').trim();
-    $(initOpenSeadragon);
-  }
-
-  if (handler) {
-    $(document).on(handler, initOpenSeadragon);
-  }
-})(jQuery);
+})();
