1332039c6e443ad31baa26c4798804bc545937ec
Justin Coyne <jcoyne@justincoyne.com>
2024-12-19 17:14:47 -0600

    Add javascript to the asset path

--- lib/openseadragon/engine.rb
+++ lib/openseadragon/engine.rb
@@ -15,6 +15,7 @@ module Openseadragon
     end
 
     initializer "openseadragon.importmap", before: "importmap" do |app|
+      app.config.assets.paths << Engine.root.join("app/javascript")
       app.config.importmap.paths << Engine.root.join("config/importmap.rb") if app.config.respond_to?(:importmap)
     end
   end
